c879a331ad5ac568c7f84a6a8727a3575a81af9c
Said Maadan <me@saidmaadan.com>
2018-05-14 10:21:05 -0500

    Added weather_list component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import WeathereReducer from './reducer_weather';
+import WeatherReducer from './reducer_weather';
 
 const rootReducer = combineReducers({
-  weather: WeathereReducer
+  weather: WeatherReducer
 });
 
 export default rootReducer;
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,5 +1,15 @@
-export default function(state = null, action){
-  console.log('Action Received:', action );
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action){
+  // console.log('Action Received:', action );
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // return state.concat([action.payload.data]);
+      return [ action.payload.data, ...state ];
+      break;
+    default:
+
+  }
 
   return state;
 }
